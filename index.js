6d0d468000ea4b14e2614dc393f5a532f48f8648
smurkio <sidegenna875@gmail.com>
2024-02-28 02:05:35 +0100

    changes

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ const cheerio = require('cheerio');
 
 const fetchAnime = async () => {
     try {
-        const response = await axios.get('https://aniwatch.info/');
+        const response = await axios.get('https://www.gogoanime.tw/');
         const html = response.data;
         const $ = cheerio.load(html);
         const animeList = []; 
-        
 
-        $('.flw-item').each((index, el) => {
+        $('.last_episodes.loaddub li').each((index, el) => {
             const anime = $(el);
-            const anime_title = anime.find('h3.film-name a').text(); 
-            const anime_url = anime.find('a').attr('href');
-            const image_url = anime.find('img').attr('data-src');
-         
-            animeList.push({ anime_title, anime_url, image_url });
+            const anime_title = anime.find('.name a').text().trim(); 
+            const anime_url = anime.find('.name a').attr('href');
+            const image_url = anime.find('img').attr('src');
+
+
+            // Extract anime ID from anime_url
+            const anime_id = anime_url.split('/').pop(); 
+
+            animeList.push({ anime_title, anime_url, anime_id, image_url });
         });
         return animeList;
     } catch (err) {
@@ -24,34 +27,5 @@ const fetchAnime = async () => {
 };
 
 fetchAnime().then(anime => console.log(anime));
-
-
-
-const fetchdetails = async () => {
-    try {
-        const response = await axios.get('https://aniwatch.info/');
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const animedetails = []; 
-        
-
-        $('.').each((index, el) => {
-            const anime = $(el);
-            const anime_title = anime.find('h3.film-name a').text(); 
-            const anime_url = anime.find('a').attr('href');
-            const image_url = anime.find('img').attr('data-src');
-         
-            animeList.push({ anime_title, anime_url, image_url });
-        });
-        return animeList;
-    } catch (err) {
-        console.error(err);
-    }
-};
-
-fetchdetail().then(anime => console.log(anime));
-
-
-
 
 
