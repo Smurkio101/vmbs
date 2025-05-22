08c7c4f24d0e9162396d1ceb97a1bf8ed5765a98
smurkio <sidegenna875@gmail.com>
2025-05-21 22:35:23 -0600

    chnages

--- index.js
+++ index.js
@@ -1,47 +1,401 @@
-const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
-const cors = require('cors');
+const express = require('express');
+const puppeteer = require('puppeteer');
+const stream = require('stream');
 
 const app = express();
 const PORT = 3001;
-
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use((req, res, next) => {
-  console.log(`Received request: ${req.method} ${req.originalUrl}`);
-  next();
+    console.log(`Received request: ${req.method} ${req.originalUrl}`);
+    next();
 });
-
-app.get('/search', async (req, res) => {
-  const keyword = req.query.keyword;
-  if (!keyword) {
-    return res.status(400).json({ error: 'Missing keyword parameter' });
-  }
-
-  try {
-    const url = `https://embtaku.pro/search.html?keyword=${encodeURIComponent(keyword)}`;
-    const response = await axios.get(url);
-    const $ = cheerio.load(response.data);
-
-    const results = [];
-    $('ul.listing.items li.video-block').each((index, element) => {
-      const anime_title = $(element).find('div.name').text().trim();
-      const anime_img = $(element).find('div.picture img').attr('src');
-      const anime_url = $(element).find('a').attr('href');
-      const anime_date = $(element).find('.date').text().trim();
-      const anime_id = anime_url.split('/').pop();
-      const anime_d = anime_date.split('-')[0];
-      results.push({ anime_title, anime_img,anime_date, anime_id, anime_d});
-    });
-
-    res.json(results);
-  } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Internal server error', message: error.message });
-  }
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 app.listen(PORT, () => console.log(`Running Express Server on PORT ${PORT}!`));
+
+
+app.get('/', (req, res) => {
+    res.status(200).json('suck yo mada {OTF, OGT, VIP}  ');
+});
+
+app.get('/animeList', async (request, response) => {
+    try {
+        const animeList = await fetchAnimeList();
+        response.send(animeList);
+    } catch (error) {
+        console.log(html);
+        console.error(error);
+        response.status(500).send('Internal Server Error');
+    }
+});
+app.get('/animedub', async (request, response) => {
+    try {
+        const animedub = await fetchDubbedAnimeList();
+        response.send(animedub);
+    } catch (error) {
+        console.error(error); // Corrected to remove the undefined reference
+        response.status(500).send('Internal Server Error');
+    }
+});
+
+
+app.get('/animeMovie', async (request, response) => {
+    try {
+        const animeMovie = await fetchAnimeMovies();
+        response.send(animeMovie);
+    } catch (error) {
+        console.error(error);
+        response.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/animePopular', async (request, response) => {
+    try {
+        const animePopular = await fetchPopularAnime();
+        response.send(animePopular);
+    } catch (error) {
+        console.error(error);
+        response.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/watch/:slug', async (request, response, next) => {
+    try {
+        const { slug } = request.params;
+        const watchDetails = await fetchWatchAndEpisodes(slug);
+        if (watchDetails) {
+            response.send(watchDetails);
+        } else {
+           
+            response.status(404).send('Content not found');
+        }
+    } catch (error) {
+        console.error(error);
+        next(error); 
+    }
+})
+
+const fetchAnimeList = async () => {
+    try {
+        const response = await axios.get('https://anitaku.to/home.html');
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const animeList = [];
+
+        $('.last_episodes.loaddub li').each((index, el) => {
+            const anime = $(el);
+            const anime_title = anime.find('.name a').text().trim();
+            const anime_url = anime.find('.name a').attr('href');
+            const image_url = anime.find('img').attr('src');
+            const anime_id = anime_url.split('/').pop();
+
+            animeList.push({ anime_title, anime_id, image_url });
+        });
+
+        
+        return animeList;
+    } catch (err) {
+        console.error(err);
+        throw err; 
+    }
+};
+
+
+
+
+const fetchAnimeMovies = async () => {
+    try {
+        const response = await axios.get('https://anitaku.to/anime-movies.html');
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const animeMovie = [];
+
+       
+        $('.last_episodes').each((index, element) => {
+            const anime = $(element);
+            const anime_title = anime.find('.name a').text().trim();
+            const anime_url = anime.find('.name a').attr('href');
+            const image_url = anime.find('img').attr('src');
+            const released = anime.find('released').text;
+
+            const anime_id = anime_url.split('/').pop();
+
+            animeMovie.push({ anime_title, anime_url, anime_id, image_url, released });
+        });
+
+        return animeMovie;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+};
+
+
+
+const fetchPopularAnime = async () => {
+    const baseUrl = 'https://anitaku.so/popular.html';
+    let currentPage = 0;
+    const totalPages = 5; // Assuming there are 5 pages, adjust this as needed
+    const animePopular = [];
+
+    try {
+        while (currentPage <= totalPages) {
+            const response = await axios.get(`${baseUrl}?page=${currentPage}`);
+            const html = response.data;
+            const $ = cheerio.load(html);
+            
+            $('.last_episodes.loaddub li').each((index, element) => {
+                const anime = $(element);
+                const anime_title = anime.find('.name a').text().trim();
+                const anime_url = anime.find('.name a').attr('href');
+                const image_url = anime.find('img').attr('src');
+                // Ensure that .text() is called as a function to get the text
+                const anime_released = anime.find('.released').text();
+
+                const anime_id = anime_url.split('/').pop();
+
+                animePopular.push({ anime_title, anime_id, image_url, anime_released });
+            });
+
+            currentPage++;
+        }
+
+        return animePopular;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+};
+
+// Example usage
+fetchPopularAnime().then(animePopular => {
+    console.log(animePopular);
+}).catch(error => {
+    console.error("Failed to fetch popular anime:", error);
+});
+
+
+
+
+
+const fetchWatchAndEpisodes = async (baseSlug) => {
+    try {
+        // Initial setup to determine if we are working with a sub or dub
+        let isDub = baseSlug.includes('-dub-');
+        let slugBaseWithoutNumber = baseSlug.split('-').slice(0, -1).join('-');
+        const initialUrl = `https://anitaku.to/${encodeURIComponent(baseSlug)}`;
+
+        // Fetch the initial page to determine total episodes and check for dub availability
+        let response = await axios.get(initialUrl);
+        let html = response.data;
+        let $ = cheerio.load(html);
+        let epRange = $('#episode_page a.active').attr('ep_end');
+        let totalEpisodes = parseInt(epRange, 10) + 1; // Assuming ep_end is inclusive
+
+        // If not initially checking for a dub, check if a dub version is available
+        if (!isDub) {
+            const dubCheckSlug = slugBaseWithoutNumber + '-dub-episode-1';
+            const dubCheckUrl = `https://anitaku.to/${encodeURIComponent(dubCheckSlug)}`;
+            try {
+                response = await axios.get(dubCheckUrl);
+                if (response.status === 200) {
+                    // Dub exists, adjust the baseSlug and slugBaseWithoutNumber for dub episodes
+                    isDub = true;
+                    baseSlug = dubCheckSlug;
+                    slugBaseWithoutNumber += '-dub';
+                }
+            } catch (error) {
+                // If the dub check fails (e.g., 404 error), continue with the original baseSlug
+            }
+        }
+
+        // Fetch episodes (either dub or sub based on the check)
+        const episodes = [];
+        for (let i = 1; i <= totalEpisodes; i++) {
+            const episodeSlug = `${slugBaseWithoutNumber}-episode-${i}`;
+            const name = `${isDub ? 'Dub' : 'Sub'} Ep ${i}`;
+
+            episodes.push({
+                episodeSlug,
+                name, // Includes whether it's a Dub or Sub
+            });
+        }
+
+        // Assuming embedLink extraction remains the same
+        const video_id = $('.play-video iframe').attr('src');
+       // const video_id = embedLink.split('/').pop();
+        
+
+        return {
+            video_id,
+            episodes,
+            
+            
+        };
+    } catch (error) {
+        console.error('Error fetching watch and episode data:', error);
+        return null;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+const fetchDubbedAnimeList = async () => {
+    const browser = await puppeteer.launch({ headless: true }); // Set to false for debugging
+    const page = await browser.newPage();
+
+    await page.goto('https://anitaku.to/home.html');
+
+    // Click the "DUB" tab
+    await page.click('a.dub[rel="2"]');
+
+    // Wait for the dubbed anime list to load
+    await page.waitForSelector('.last_episodes.loaddub li .type.ic-DUB', { timeout: 10000 });
+
+    // Scrape the dubbed anime list
+    const animeList = await page.evaluate(() => {
+        // Only select list items that have the .ic-DUB type indicator
+        const items = Array.from(document.querySelectorAll('.last_episodes.loaddub li .type.ic-DUB')).map(typeDiv => typeDiv.closest('li'));
+        return items.map(item => {
+           
+            const titleElement = item.querySelector('p.name a');
+            const anime_title = titleElement ? titleElement.title.trim() : '';
+            const episodeElement = item.querySelector('p.episode');
+            const ep = episodeElement ? episodeElement.textContent.trim() : '';
+            const urlElement = item.querySelector('p.name a');
+            const anime_url = urlElement ? new URL(urlElement.href, window.location.origin).href : '';
+            const imageElement = item.querySelector('.img img');
+            const image_Url = imageElement ? new URL(imageElement.src, window.location.origin).href : '';
+            const anime_id = anime_url.split('/').pop();
+
+            return { anime_title, ep, image_Url , anime_id};
+        });
+    });
+
+    await browser.close();
+    return animeList;
+};
+
+fetchDubbedAnimeList()
+    .then(animeList => console.log(animeList))
+    .catch(error => console.error(error));
+
+
+
+
+
+
+
+
+async function fetchAnni(slug) {
+    try {
+        const url = `https://www.cbr.com/${slug}`;
+        const { data } = await axios.get(url);
+        const $ = cheerio.load(data);
+
+        const content = $('#article-body .content-block-regular p')
+            .map((i, el) => $(el).text().trim())
+            .get()
+            .join(' '); // Joining all the paragraphs into a single string
+
+        const mangaDetails = {
+            anime_title: $('.article-header-title').text().trim(),
+            tag: $('.tag-label.no-bg span').text().trim(),
+            img: $('.heading_image img').attr('src'),
+            description: $('meta[name="description"]').attr('content').trim(),
+            content: content
+        };
+
+        return mangaDetails;
+    } catch (error) {
+        console.error('Error fetching the page:', error);
+        throw error;
+    }
+}
+// Express route handler
+app.get('/news/:slug', async (request, response, next) => {
+    try {
+        const { slug } = request.params;
+        const anniDetails = await fetchAnni(slug);
+
+        if (anniDetails) {
+            response.json(anniDetails);
+        } else {
+            response.status(404).send('Content not found');
+        }
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+
+app.get('/uie', async (req, res) => {
+    const newsList = await scrapeAllNewsPages();
+    res.json(newsList);
+});
+
+
+const fetchLatestNewsFromPage = async (url) => {
+    try {
+        const response = await axios.get(url);
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const newsList = [];
+
+        $('.display-card.article.small.active-content').each((index, el) => {
+            const newsElement = $(el);
+            const anime_title = newsElement.find('.display-card-title').text().trim();
+            const  anime_image = newsElement.find('picture source').last().attr('srcset');
+            const anime_url = newsElement.find('a.dc-img-link').attr('href');
+            const anime_id = anime_url.replace(/^\/|\/$/g, '');
+
+            newsList.push({
+                anime_title,
+                anime_image,
+                anime_id,
+            });
+        });
+
+        return newsList;
+    } catch (err) {
+        console.error(err);
+        return [];
+    }
+}
+
+
+
+const scrapeAllNewsPages = async () => {
+    const baseUrl = 'https://www.cbr.com/category/anime/';
+    const maxPages = 3;  // Total number of pages you want to scrape
+    const allNews = [];
+
+    for (let page = 1; page <= maxPages; page++) {
+        const url = page === 1 ? baseUrl : `${baseUrl}${page}/`;
+        const newsList = await fetchLatestNewsFromPage(url);
+        if (newsList.length > 0) {
+            allNews.push(...newsList);
+        } else {
+            break;  // Stop the loop if no news is found on a page
+        }
+    }
+
+    return allNews;
+}
+
+
